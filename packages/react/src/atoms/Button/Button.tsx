dbc27cbfe1f6fe0c16fe7b7a4716ed37c6f2d853
BrunnoM7 <brunno.marchetti7@gmail.com>
2022-01-17 17:45:48 -0300

    feat(playground): set up the playground environment

--- packages/react/src/atoms/Button/Button.tsx
+++ packages/react/src/atoms/Button/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 const Button: React.FunctionComponent<ButtonProps> = ({ label }) => {
   return (
-    <button>{label}</button>
+    <button className='bmds-button__container'>{label}</button>
   )
 }
 
