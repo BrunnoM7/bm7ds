8bf241f1ce590bc9c635fa99ddd58855f8adc5c8
BrunnoM7 <brunno.marchetti7@gmail.com>
2022-02-15 16:01:14 -0300

    feat(Select): finished base implemantation of Select molecule

--- packages/react/src/molecules/Select/Select.tsx
+++ packages/react/src/molecules/Select/Select.tsx
@@ -1,5 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, KeyboardEventHandler, createRef } from 'react';
 import Text from '../../atoms/Text';
+
+const KEYS = {
+  ENTER: "Enter",
+  SPACE_BAR: " ",
+  DOWN_ARROW: "ArrowDown",
+  UP_ARROW: "ArrowUp",
+  ESC: "Escape"
+}
 
 interface SelectOption {
   label: string
@@ -19,16 +27,51 @@ interface SelectProps {
   renderOption?: (props: RenderOptionProps) => React.ReactNode
 }
 
+const getNextOptionIndex = (currentIndex: number|null, options: Array<SelectOption>) => {
+  if (currentIndex === null) {
+    return 0;
+  }
+  if (currentIndex === options.length - 1) {
+    return 0;
+  }
+  return currentIndex + 1;
+}
+
+const getPreviousOptionIndex = (currentIndex: number|null, options: Array<SelectOption>) => {
+  if (currentIndex === null) {
+    return 0;
+  }
+  if (currentIndex === 0) {
+    return options.length - 1;
+  }
+  return currentIndex - 1;
+}
+
 const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'Select an option...', onOptionSelected: handler, renderOption }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [selectedIndex, setSelectedIndex] = useState<number|null>(null);
+  const [highlightedIndex, setHighlightedIndex] = useState<number|null>(null);
   const [overlayTop, setOverlayTop] = useState<number>(0);
+  const [optionRefs, setOptionRefs] = useState<React.RefObject<HTMLLIElement>[]>([])
   const labelRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     setOverlayTop((labelRef.current?.offsetHeight || 0) + 10)
-  }, [labelRef.current?.offsetHeight])
+  }, [labelRef.current?.offsetHeight]);
+
+  useEffect(() => {
+    setOptionRefs(options.map(_ => createRef<HTMLLIElement>()))
+  }, [options.length]);
   
+  useEffect(() => {
+    if(highlightedIndex !== null && isOpen) {
+      const ref = optionRefs[highlightedIndex];
+
+      if(ref && ref.current) {
+        ref.current.focus();
+      }
+    }
+  }, [isOpen, highlightedIndex])
 
   const onOptionSelected = (option: SelectOption, optionIndex: number) => {
     setIsOpen(!isOpen);
@@ -42,13 +85,55 @@ const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'S
     setIsOpen(!isOpen);
   }
 
+  const highlightOption = (optionIndex: number|null) => {
+    setHighlightedIndex(optionIndex);
+  }
+
+  const onButtonKeyDown: KeyboardEventHandler = (event) => {
+    event.preventDefault();
+
+    if([KEYS.ENTER, KEYS.SPACE_BAR, KEYS.DOWN_ARROW].includes(event.key)) {
+      setIsOpen(true);
+
+      // set focus on the list item
+      highlightOption(0);
+    }
+  }
+
+  const onOptionKeyDown: KeyboardEventHandler = (event) => {
+    if(event.key === KEYS.ESC) {
+      setIsOpen(false);
+
+      return
+    }
+
+    if(event.key === KEYS.DOWN_ARROW) {
+      highlightOption(getNextOptionIndex(highlightedIndex, options));
+    }
+    if(event.key === KEYS.UP_ARROW) {
+      highlightOption(getPreviousOptionIndex(highlightedIndex, options));
+    }
+    if(event.key === KEYS.ENTER) {
+      onOptionSelected(options[highlightedIndex!], highlightedIndex!);
+    }
+  }
+
   let selectedOption = null;
   if(selectedIndex !== null) {
     selectedOption = options[selectedIndex];
   }
 
   return <div className='bmds-select'>
-    <button ref={labelRef} className='bmds-select__label' onClick={() => onLabelClick()}>
+    <button 
+      className='bmds-select__label' 
+      ref={labelRef} 
+      aria-haspopup={true} 
+      aria-expanded={isOpen? true : undefined} 
+      aria-controls='bmds-select-list'
+      onClick={() => onLabelClick()}
+      onKeyDown={onButtonKeyDown}
+      tabIndex={0}
+    >
       <Text>{selectedOption === null ? label : selectedOption.label }</Text>
       
       {/* 
@@ -61,20 +146,37 @@ const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'S
     </button>
 
     {isOpen ? 
-      <ul style={{ top: overlayTop }} className='bmds-select__overlay'>
+      <ul 
+        role='menu' 
+        id='bmds-select-list' 
+        style={{ top: overlayTop }} 
+        className='bmds-select__overlay'
+      >
         {options.map((option, optionIndex) => {
           const isSelected = selectedIndex === optionIndex;
+          const isHighlighted = highlightedIndex === optionIndex;
+
+          const ref = optionRefs[optionIndex];
 
           const renderOptionProps = {
             option,
             isSelected,
             getOptionRecommendedProps: (overrideProps = {}) => {
               return {
+                ref,
+                role: 'menuitemradio',
+                'aria-label': option.label,
+                'aria-checked': isSelected ? true : undefined,
+                tabIndex: isHighlighted ? -1 : 0,
                 className: `bmds-select__option
                   ${isSelected ? 'bmds-select__option--selected' : ''}
+                  ${isHighlighted ? 'bmds-select__option--highlighted' : ''}
                 `,
                 key: option.value,
                 onClick: () => onOptionSelected(option, optionIndex),
+                onMouseEnter: () => highlightOption(optionIndex),
+                onMouseLeave: () => highlightOption(null),
+                onKeyDown: onOptionKeyDown,
                 ...overrideProps
               }
             }
@@ -85,12 +187,7 @@ const Select: React.FunctionComponent<SelectProps> = ({ options = [], label = 'S
           }
           return (
             <li 
-              className={`bmds-select__option
-                ${isSelected ? 'bmds-select__option--selected' : ''}
-              `}
-              onClick={() => onOptionSelected(option, optionIndex)} 
-              value={option.value} 
-              key={option.value}
+              {...renderOptionProps.getOptionRecommendedProps()}
             >
               <Text>
                 {option.label}
