8a3a370d4bb23dc8d2b23eafedc65e82bbd1e3e3
BrunnoM7 <brunno.marchetti7@gmail.com>
2022-02-17 21:50:09 -0300

    test(Select): implemented test suit for Select molecule

--- packages/react/src/molecules/Select/Select.test.tsx
+++ packages/react/src/molecules/Select/Select.test.tsx
@@ -9,8 +9,8 @@ const options = [
     value: "strict-black"
   },
   {
-    label: 'Black',
-    value: "black"
+    label: 'Black Lagoon',
+    value: "black-lagoon"
   },
   {
     label: 'Sirius Black',
@@ -26,22 +26,61 @@ test('renders all options passed to it', () => {
   expect(getAllByRole('menuitemradio')).toHaveLength(options.length);
 })
 
+test('can customize select label', () => {
+  const { getByText } = render(<Select options={options} label='CUSTOM LABEL'/>);
+
+  expect(getByText('CUSTOM LABEL')).toBeInTheDocument()
+})
+
 test('renders options using custom renderOption method if passed as prop', () => {
-  // TODO
+  const { getAllByTestId, getByTestId } = render(<Select options={options} renderOption={({ option, getOptionRecommendedProps }) => {
+    return <li data-testid='CustomRenderOption' { ...getOptionRecommendedProps() }>{option.label}</li>
+  }} />)
+
+  fireEvent.click(getByTestId('BmdsSelectButton'));
+
+  expect(getAllByTestId('CustomRenderOption')).toHaveLength(options.length)
 })
 
 test('calls the onOptionSelected prop with the selected option and its index if passed', () => {
-  // TODO
+  const onOptionSelected = jest.fn();
+  const { getAllByRole, getByTestId } = render(<Select options={options} onOptionSelected={onOptionSelected}/>);
+  
+  fireEvent.click(getByTestId('BmdsSelectButton'));
+  fireEvent.click(getAllByRole('menuitemradio')[0]);
+
+  expect(onOptionSelected).toHaveBeenCalledWith(options[0], 0);
 })
 
 test('the button label changes to the selected option label', () => {
-  // TODO
+  const { getAllByRole, getByTestId } = render(<Select options={options} />);
+  
+  fireEvent.click(getByTestId('BmdsSelectButton'));
+  fireEvent.click(getAllByRole('menuitemradio')[0]);
+
+  expect(getByTestId('BmdsSelectButton')).toHaveTextContent(options[0].label);
+  
 })
 
 test('snapshot of the selected option state', () => {
-  // TODO
+  const { getAllByRole, getByTestId, asFragment } = render(<Select options={options} />);
+  
+  fireEvent.click(getByTestId('BmdsSelectButton'));
+  fireEvent.click(getAllByRole('menuitemradio')[0]);
+
+  expect(asFragment()).toMatchSnapshot();
+})
+
+test('snapshot of the base state', () => {
+  const { asFragment } = render(<Select options={options} />);
+
+  expect(asFragment()).toMatchSnapshot();
 })
 
 test('snapshot of the options menu open state', () => {
-  // TODO
+  const { getByTestId, asFragment } = render(<Select options={options} />);
+  
+  fireEvent.click(getByTestId('BmdsSelectButton'));
+
+  expect(asFragment()).toMatchSnapshot();
 })
