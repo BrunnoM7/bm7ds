4b5ddcbf8e6ab19900c2f052375f099d271976e1
BrunnoM7 <brunno.marchetti7@gmail.com>
2022-02-18 19:23:15 -0300

    fix(storybook): added css suport to storybook

--- packages/react/src/molecules/Select/__stories__/Select.stories.tsx
+++ packages/react/src/molecules/Select/__stories__/Select.stories.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import Select from "../Select";
 
+import '@bmds/scss/lib/global.css'
+import '@bmds/scss/lib/Select.css'
+
 export default {
   title: 'Select'
 }
 
-export const Common = () => <Select options={[]} />
+export const Common = () => <Select options={options} />
+
+const options =[
+  {
+    label: 'Strict Black',
+    value: "strict-black"
+  },
+  {
+    label: 'Black',
+    value: "black"
+  },
+  {
+    label: 'Sirius Black',
+    value: "sirius-black"
+  },
+]
